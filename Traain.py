2aef9807b9e3f624b48b600996787091a961dff4
AlexFRUZ <olexandrgladungmail.com>
2024-04-02 18:26:44 +0300

    Last update

--- Traain.py
+++ Traain.py
@@ -1,16 +1,67 @@
 import cv2
 from keras.models import model_from_json
 import numpy as np
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QSplitter
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QSplitter, QMessageBox
 from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtGui import QImage, QPixmap
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
+
 def extract_features(image):
     feature = np.array(image)
     feature = feature.reshape(1, 48, 48, 1)
     return feature / 255.0
+
+
+class PositiveEmotionWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Emotion Levels')
+        self.resize(600, 500)
+
+        self.figure, self.ax = plt.subplots()
+        self.emotion_line, = self.ax.plot([], [], 'b-', label='Emotion Levels')
+        self.ax.set_xticks(range(7))
+        self.ax.set_xticklabels(['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise'])
+        self.ax.set_ylabel('Emotion Level')
+        self.ax.set_title('Emotion Levels over Time')
+        self.ax.legend()
+
+        self.canvas = FigureCanvas(self.figure)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+
+        self.x_data = []
+        self.y_data = []
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start(1000)
+
+    def update_plot(self):
+        positive_emotion = 10
+        negative_emotion = 5
+        neutral_emotion = 3
+
+        total_emotion = positive_emotion - negative_emotion
+        if total_emotion > 0:
+            position = 1
+        elif total_emotion < 0:
+            position = -1
+        else:
+            position = 0
+
+        self.x_data.append(len(self.x_data) + 1)
+        self.y_data.append(position)
+
+        self.emotion_line.set_data(self.x_data, self.y_data)
+        self.ax.relim()
+        self.ax.autoscale_view(True, True, True)
+        self.canvas.draw()
+
 
 class EmotionDetectorApp(QWidget):
     def __init__(self):
@@ -23,6 +74,7 @@ class EmotionDetectorApp(QWidget):
         self.emotion_counts = None
         self.statistics_label = None
         self.btn_stat = None
+        self.btn_positive = None
         self.webcam = None
         self.labels = None
         self.haar_file = None
@@ -30,6 +82,7 @@ class EmotionDetectorApp(QWidget):
         self.model = None
         self.model_json = None
         self.json_file = None
+        self.positive_window = None
         self.initUI()
 
     def initUI(self):
@@ -48,8 +101,11 @@ class EmotionDetectorApp(QWidget):
         self.btn_stat = QPushButton('Show Emotion Statistics', self)
         self.btn_stat.clicked.connect(self.show_statistics)
 
+        self.btn_positive = QPushButton('Show Positive Emotions', self)
+        self.btn_positive.clicked.connect(self.show_positive_emotions)
+
         self.statistics_label = QLabel(self)
-        self.emotion_counts = {label: 0 for label in self.labels.values()}  # Initialize counts for each emotion
+        self.emotion_counts = {label: 0 for label in self.labels.values()}
 
         self.figure, self.ax = plt.subplots()
         self.canvas = FigureCanvas(self.figure)
@@ -63,6 +119,7 @@ class EmotionDetectorApp(QWidget):
 
         layout = QVBoxLayout()
         layout.addWidget(self.btn_stat)
+        layout.addWidget(self.btn_positive)
         layout.addWidget(self.splitter)
 
         self.setLayout(layout)
@@ -103,15 +160,39 @@ class EmotionDetectorApp(QWidget):
         except cv2.error:
             pass
 
+    def show_positive_emotions(self):
+        self.positive_window = PositiveEmotionWindow()
+        self.positive_window.show()
+
     def show_statistics(self):
         self.ax.clear()
         self.ax.bar(self.labels.values(), self.emotion_counts.values())
         self.ax.set_ylabel('Count')
         self.ax.set_title('Emotion Statistics')
         self.canvas.draw()
+        dominant_emotion = max(self.emotion_counts, key=self.emotion_counts.get)
+        if dominant_emotion == 'angry':
+            message = "You seem to be angry."
+        elif dominant_emotion == 'disgust':
+            message = "You seem to be disgusted."
+        elif dominant_emotion == 'fear':
+            message = "You seem to be afraid."
+        elif dominant_emotion == 'happy':
+            message = "You seem to be happy."
+        elif dominant_emotion == 'neutral':
+            message = "You seem to be neutral."
+        elif dominant_emotion == 'sad':
+            message = "You seem to be sad."
+        elif dominant_emotion == 'surprise':
+            message = "You seem to be surprised."
+        else:
+            message = "Unable to determine your emotion."
+
+        QMessageBox.information(self, "Dominant Emotion", message)
 
 
 if __name__ == '__main__':
     app = QApplication([])
     window = EmotionDetectorApp()
+    window.show()
     app.exec_()
