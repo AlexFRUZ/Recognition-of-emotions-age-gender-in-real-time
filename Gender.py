aea53f7c859c21a009760830c1c7269b4578078f
AlexFRUZ <olexandrgladungmail.com>
2024-06-03 15:18:29 +0300

    Add 2 layers

--- Gender.py
+++ Gender.py
@@ -7,18 +7,19 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 from sklearn.preprocessing import LabelEncoder
-from PIL import Image
+
 
 num_classes = 2
 
 TRAIN_DIR = 'dataset'
+
 
 def create_dataframe(directory):
     image_path = []
     labels = []
     for label in os.listdir(directory):
         label_path = os.path.join(directory, label)
-        if os.path.isdir(label_path):  # Перевірка, чи це дійсно каталог
+        if os.path.isdir(label_path):
             for image_name in os.listdir(label_path):
                 image_path.append(os.path.join(label_path, image_name))
                 labels.append(label)
@@ -27,11 +28,12 @@ def create_dataframe(directory):
 train = pd.DataFrame()
 train['image'], train['label'] = create_dataframe(TRAIN_DIR)
 
+
 def extract_features(images):
     features = []
     for image in tqdm(images):
         img = load_img(image, grayscale=True)
-        img = img.resize((48, 48))  # Змінено розмір до 48x48 пікселів
+        img = img.resize((48, 48))
         img = img_to_array(img)
         features.append(img)
     features = np.array(features)
@@ -47,12 +49,19 @@ y_train = le.fit_transform(train['label'])
 y_train = to_categorical(y_train, num_classes)
 
 model = Sequential()
+
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.2))
+model.add(Dropout(0.4))
+
+model.add(Conv2D(512, kernel_size=(3, 3), activation='relu'))
+model.add(Dropout(0.3))
+
 model.add(Flatten())
+
 model.add(Dense(num_classes, activation='softmax'))
+
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(x=x_train, y=y_train, batch_size=32, epochs=10, validation_split=0.2)
+model.fit(x=x_train, y=y_train, batch_size=128, epochs=40, validation_split=0.2)
 
 model.save('gender.h5')
